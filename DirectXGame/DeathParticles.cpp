0033f4dd4d04b81f68c38628ddc6f767cbc06eb1
k023g1018-hidemasa <147122260+k023g1018-hidemasa@users.noreply.github.com>
2024-07-11 19:05:04 +0900

    ittannup

---
+++ DirectXGame/DeathParticles.cpp
@@ -0,0 +1,17 @@
+#include "DeathParticles.h"
+#include"WorldTransform.h"
+
+void DeathParticles::Initialize(Model* DeathParticlesModel, ViewProjection* viewProjection, const Vector3& position) {
+	DeathParticlesModel_ = DeathParticlesModel;
+	// 行列を定数バッファに転送//定数バッファ＝グラボ
+	viewProjection_ = viewProjection;
+	for (auto& worldTransform : worldTransforms_) {
+		worldTransform.Initialize();//whay
+		worldTransform.translation_ = position;
+	}
+
+
+}
+void DeathParticles::Update() {}
+
+void DeathParticles::Draw() {}
---
+++ DirectXGame/DeathParticles.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "Model.h"
+#include "Vector3.h"
+#include "ViewProjection.h"
+#include<array>
+
+class DeathParticles {
+public:
+	
+	void Initialize(Model* model, ViewProjection* viewProjection, const Vector3& position);
+	/// <summary>
+	/// 更新処理
+	/// </summary>
+	void Update();
+	/// <summary>
+	/// 描画処理
+	/// </summary>
+	void Draw();
+
+private:
+	Model* DeathParticlesModel_ = nullptr;
+	ViewProjection* viewProjection_ = nullptr;
+	//パテの個数
+	static inline const uint32_t kNumParticles = 8;
+
+	std::array<WorldTransform, kNumParticles> worldTransforms_;
+};
